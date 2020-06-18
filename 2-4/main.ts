707df0643fb3d5d92c916b5c219a06e3bf610bc9
makotoyasuda <>
2020-06-18 23:14:30 +0900

    create 2-4 question

---
+++ 2-4/main.ts
@@ -0,0 +1,28 @@
+
+let getMonth:number = Number(process.argv[2]);
+
+switch (getMonth) {
+  case 2:
+  case 3:
+  case 4:
+    console.log("春");
+    break;
+  case 5:
+  case 6:
+  case 7:
+    console.log("夏");
+    break;
+  case 8:
+  case 9:
+  case 10:
+    console.log("秋");
+    break;
+  case 11:
+  case 12:
+  case 1:
+    console.log("冬");
+    break;
+  default:
+    console.log("1-12までの数字を入力してください");
+    break;
+}
